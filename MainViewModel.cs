ef8661ca2fc1f3f6f248f33ff2e8838254581da6
Ingrid Gabriel <jacquelinegabrielz@outlook.com>
2023-07-26 07:11:35 -0600

    se implemento un listview

---
+++ MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MvvmHelpers;
+using System.Collections.ObjectModel;
+using static Marthas_Coffee_App_Movil.Pages.Pedido;
+using Microsoft.Maui.Controls;
+
+
+namespace Marthas_Coffee_App_Movil
+{
+    public class MainViewModel : BaseViewModel
+    {
+       private ObservableCollection<CPedido> _listproducts;
+        public ObservableCollection<CPedido> ListProducts
+        {
+            get { return _listproducts; }
+            set { SetProperty(ref _listproducts, value); }
+        }
+       //public List<DetailsView> ListProducts { get; set; } 
+
+        public MainViewModel()
+        {
+           ListProducts = new ObservableCollection<CPedido>
+            {
+                new CPedido { Platillos = "Ensalada", Bebidas = "Agua", Postres = "Helado", Bocadillos = "Galletas" },
+                new CPedido { Platillos = "Pizza", Bebidas = "Refresco", Postres = "Pastel", Bocadillos = "Nachos" }
+            };
+
+            // Agrega más productos aquí 
+        }
+    }
+}
--- Pages/Pedido.xaml.cs
+++ Pages/Pedido.xaml.cs
@@ -1,9 +1,20 @@
-namespace Marthas_Coffee_App_Movil.Pages;
 
+
+namespace Marthas_Coffee_App_Movil.Pages;
 public partial class Pedido : ContentPage
 {
-	public Pedido()
-	{
+    public class CPedido
+    {
+        public string Platillos { get; set; }
+        public string Bebidas { get; set; }
+        public string Bocadillos { get; set; }
+        public string Postres { get; set; }
+
+    }
+    public Pedido()
+	{ 
 		InitializeComponent();
-	}
+        BindingContext = new MainViewModel();
+
+    }
 }
