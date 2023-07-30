7faaaa00c20b1a58a35c6271b1e51374875f9f8d
yeraldycarpio <2430933@clases.edu.sv>
2023-07-30 11:45:05 -0600

    Reedición 1

--- Pages/Cuenta.xaml.cs
+++ Pages/Cuenta.xaml.cs
@@ -1,3 +1,5 @@
+using Marthas_Coffee_App_Movil.Pages.Categorias.Cuenta;
+
 namespace Marthas_Coffee_App_Movil.Pages;
 
 public partial class Cuenta : ContentPage
@@ -5,5 +7,15 @@ public partial class Cuenta : ContentPage
 	public Cuenta()
 	{
 		InitializeComponent();
+	}
+
+	private void sesion_Clicked(object sender, EventArgs e)
+	{
+
+	}
+
+	private void Crear_Clicked(object sender, EventArgs e)
+	{
+		Navigation.PushModalAsync(new CrearCuenta());
 	}
 }
