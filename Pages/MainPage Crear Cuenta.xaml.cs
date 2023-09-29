f53c2ac2bc9da04a01de9937485c7a05fc57bf0b
IngridGbrl <5330038@clases.edu.sv>
2023-09-29 11:45:09 -0600

    se empezo a trabajar en el login

--- Pages/MainPage Crear Cuenta.xaml.cs
+++ Pages/MainPage Crear Cuenta.xaml.cs
@@ -1,3 +1,7 @@
+using Marthas_Coffee_App_Movil.Tables;
+using Network;
+using SQLite;
+
 namespace Marthas_Coffee_App_Movil.Pages;
 
 public partial class MainPage_Crear_Cuenta : ContentPage
@@ -11,5 +15,17 @@ public partial class MainPage_Crear_Cuenta : ContentPage
     {
 
         await Navigation.PushModalAsync(new MainPage_Cuenta());
+    }
+
+    private void Registro_Clicked(object sender, EventArgs e)
+    {
+        var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UsersDatabase.bd");
+        var db = new SQLiteConnection(dbpath);
+        db.CreateTable<RegistrarUsuario>();
+
+        var u = RegistrarUsuario(){
+            Username = User.Text,
+
+        }
     }
 }
--- Pages/MainPage Cuenta.xaml.cs
+++ Pages/MainPage Cuenta.xaml.cs
@@ -1,3 +1,5 @@
+using Marthas_Coffee_App_Movil.Tables;
+using SQLite;
 namespace Marthas_Coffee_App_Movil.Pages;
 
 public partial class MainPage_Cuenta : ContentPage
@@ -9,7 +11,29 @@ public partial class MainPage_Cuenta : ContentPage
 
     private void sesion_Clicked(object sender, EventArgs e)
     {
+        var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UsersDatabase.bd");
+        var db = new SQLiteConnection(dbpath);
+        var myquery = db.Table<RegistrarUsuario>().Where(u => u.Username.Equals(credenciales.Text) && u.Password.Equals(contra.Text)).FirstOrDefault();
 
+        if(myquery != null)
+        {
+            App.Current.MainPage = new NavigationPage(new Inicio());
+        }
+        else
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Error", "Usuario o contraseña incorrecta. Revisar datos", "Yes", "Cancel");
+
+                if (result)
+                    await Navigation.PushAsync(new MainPage_Cuenta());
+                else
+                {
+                    await Navigation.PushAsync(new MainPage_Cuenta());
+                }
+            }
+            );
+        }
     }
 
     private void Crear_Clicked(object sender, EventArgs e)
